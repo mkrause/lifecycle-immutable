ecc019154b03d7178d3eca981fa411881cec9b36
mkrause <maikelkrause@gmail.com>
2018-04-04 02:45:54 +0200

    Remove orEmpty.

--- src/schema/Schema.js
+++ src/schema/Schema.js
@@ -2,8 +2,6 @@
 const Schema = {
     text: Symbol('Schema.text'),
     integer: Symbol('Schema.integer'),
-    
-    orEmpty: t => ({ $option: { empty: "", nonempty: t } }),
 };
 
 export default Schema;
